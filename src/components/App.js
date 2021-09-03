deb5e7b9ea99e4139c06d063a3e8451545341da8
1day2coffee <123rbdud@gmail.com>
2021-09-03 11:31:53 +0900

    #2.1 login form part one

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,12 @@ import { authService } from "fBase";
 
 function App() {
   const [isLoggedIn, setIsLoggedIn] = useState(authService.currentUser);
-  return <AppRouter isLoggedIn={isLoggedIn} />;
+  return (
+    <>
+      <AppRouter isLoggedIn={isLoggedIn} />
+      <footer>&copy; {new Date().getFullYear()} Nwitter</footer>
+    </>
+  );
 }
 
 export default App;
