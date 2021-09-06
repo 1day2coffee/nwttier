5395ffec1e448826233d6dd731fbe30ef035d064
1day2coffee <42764588+1day2coffee@users.noreply.github.com>
2021-09-06 09:50:33 +0900

    #3.2 getting the nweets

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,9 +1,26 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { dbService } from "fBase";
-import { addDoc, collection } from "firebase/firestore";
+import { addDoc, collection, getDocs } from "firebase/firestore";
 
 const Home = () => {
   const [nweet, setNweet] = useState("");
+  const [nweets, setNweets] = useState([]);
+
+  const getNweets = async () => {
+    const dbNweets = await getDocs(collection(dbService, "nweets"));
+    dbNweets.forEach((document) => {
+      const nweetObject = {
+        ...document.data(),
+        id: document.id,
+      };
+      setNweets((prev) => [nweetObject, ...prev]);
+    });
+  };
+  useEffect(() => {
+    getNweets();
+  }, []);
+
+
   const onSubmit = async (e) => {
   e.preventDefault();
   try {
@@ -22,6 +39,8 @@ const Home = () => {
   setNweet(value);
   };
 
+  console.log(nweets);
+
   return (
     <div>
       <form onSubmit={onSubmit}>
@@ -34,6 +53,13 @@ const Home = () => {
         />
         <input type="submit" value="Nweet" />
       </form>
+      <div>
+      {nweets.map((nweet) => (
+          <div key={nweet.id}>
+            <h4>{nweet.nweet}</h4>
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
