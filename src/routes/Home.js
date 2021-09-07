be5a6f484bcdfe3ac288bf6398a5b82181673e72
1day2coffee <123rbdud@gmail.com>
2021-09-07 09:42:36 +0900

    #4.1 preview image

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -14,25 +14,15 @@ import {
 const Home = ({ userObj }) => {
   const [nweet, setNweet] = useState("");
   const [nweets, setNweets] = useState([]);
-
-  // useEffect(() => {
-  //   dbService.collection("nweets").onSnapshot((snapshot) => {
-  //     const nweetArray = snapshot.docs.map((doc) => ({
-  //       id: doc.id,
-  //       ...doc.data(),
-  //     }));
-  //     setNweets(nweetArray);
-  //   });
-  // }, []);
+  const [attachment, setAttachment] = useState();
 
   useEffect(() => {
-    // 실시간으로 데이터를 데이터베이스에서 가져오기
-
     const q = query(
       collection(getFirestore(), "nweets"),
       // where('text', '==', 'hehe') // where뿐만아니라 각종 조건 이 영역에 때려부우면 됨
       orderBy("createdAt")
     );
+
     const unsubscribe = onSnapshot(q, (querySnapshot) => {
       const newArray = querySnapshot.docs.map((doc) => {
         return {
@@ -61,9 +51,30 @@ const Home = ({ userObj }) => {
 
     setNweet("");
   };
-  const onChange = ({ target: { value } }) => {
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+
     setNweet(value);
   };
+
+  const onFileChange = (event) => {
+    const {
+      target: { files },
+    } = event;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent) => {
+      const {
+        currentTarget: { result },
+      } = finishedEvent;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => setAttachment(null);
 
   return (
     <div>
@@ -75,15 +86,22 @@ const Home = ({ userObj }) => {
           placeholder="What's on your mind?"
           maxLength={120}
         />
+        <input type="file" accept="image/*" onChange={onFileChange} />
         <input type="submit" value="Nweet" />
+        {attachment && (
+          <div>
+            <img src={attachment} width="50px" height="50px" />
+            <button onClick={onClearAttachment}>Clear</button>
+          </div>
+        )}
       </form>
       <div>
         {nweets.map((nweet) => (
           <Nweet
-          key={nweet.id}
-          nweetObj={nweet}
-          isOwner={nweet.creatorId === userObj.uid}
-        />
+            key={nweet.id}
+            nweetObj={nweet}
+            isOwner={nweet.creatorId === userObj.uid}
+          />
         ))}
       </div>
     </div>
