8632f529a53b1609064ad4695db5ac69735648f0
JYPARK <jy.park@smartcareworks.com>
2021-09-08 15:33:46 +0900

    #5.0 get my own nweets

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,8 +2,8 @@ import React from "react";
 import { HashRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "routes/Home";
 import Auth from "routes/Auth";
-import Profile from "routes/Profile";
 import Navigation from "components/Navigation";
+import Profile from "routes/Profile";
 
 const AppRouter = ({ isLoggedIn, userObj }) => {
   return (
@@ -16,7 +16,7 @@ const AppRouter = ({ isLoggedIn, userObj }) => {
               <Home userObj={userObj} />
             </Route>
             <Route exact path="/profile">
-              <Profile />
+              <Profile userObj={userObj} />
             </Route>
           </>
         ) : (
--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -1,19 +1,39 @@
-import React from "react";
-import {authService} from "fBase";
+import React, { useEffect } from "react";
+import { authService, dbService } from "fBase";
 import { useHistory } from "react-router-dom";
+import {
+  collection,
+  orderBy,
+  query,
+  where,
+  getDocs,
+} from "@firebase/firestore";
 
-export default () => {
-
+export default ({ userObj }) => {
   const history = useHistory();
   const onLogOutClick = () => {
     authService.signOut();
     history.push("/");
   };
 
+  const getMyNweets = async () => {
+    const nweets = await getDocs(
+      query(
+        collection(dbService, "nweets"),
+        where("creatorId", "==", userObj.uid),
+        orderBy("createdAt")
+      )
+    );
+    console.log(nweets.docs.map((doc) => doc.data()));
+  };
+
+  useEffect(() => {
+    getMyNweets();
+  }, []);
+
   return (
     <>
       <button onClick={onLogOutClick}>Log Out</button>
     </>
   );
-
 };
